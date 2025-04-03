9db8c11b0864492224419ac3a7ea744819e85dc8
Kamil Piejko <173197@stud.prz.edu.pl>
2025-04-04 01:52:27 +0200

    Usunięto integrację z CAS(dodamy na pozniejszym etapie realizacji projektu) przywrócono logowanie przez baze - nalezy dodac uzytkownikow i role w nastepnym kroku

--- users/views.py
+++ users/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
-    return render(request, 'home.html', {'message': 'Witaj!'})
+    menu_items = {
+        'ADMIN': [
+            {'name': 'Zarządzanie użytkownikami', 'url': '/admin/'},
+            {'name': 'Sekcja administracyjna', 'url': '#'},
+        ],
+        'PRAWNIK': [
+            {'name': 'Dokumenty prawne', 'url': '#'},
+        ],
+        'OBSLUGA': [
+            {'name': 'Obsługa wniosków', 'url': '#'},
+        ],
+        'RADA': [
+            {'name': 'Posiedzenia rady', 'url': '#'},
+        ],
+    }
+    user_role = request.user.role
+    context = {
+        'message': 'Witaj w SIAU!',
+        'menu': menu_items.get(user_role, []),
+    }
+    return render(request, 'home.html', context)
