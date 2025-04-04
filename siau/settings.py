8ddd226b84d4888cd1839ea49c53a404ca4ee92e
Kamil Piejko <173197@stud.prz.edu.pl>
2025-04-04 02:20:25 +0200

    błąd z zajętym portem został rozwiązany, lsof -i :nr_portu, kill -9 nr_portu

--- siau/settings.py
+++ siau/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'templates']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -130,3 +131,4 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+
